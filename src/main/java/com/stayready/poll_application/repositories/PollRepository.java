0349db90f2bfa8c1579b0cac4880430d8f094c29
Faith Wilkins El <fwilkins@terpmail.umd.edu>
2020-08-06 17:52:42 -0400

    Following Tariq

--- src/main/java/com/stayready/poll_application/repositories/PollRepository.java
+++ src/main/java/com/stayready/poll_application/repositories/PollRepository.java
@@ -2,6 +2,7 @@ package com.stayready.poll_application.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import com.stayready.poll_application.domain.*;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface PollRepository extends PagingAndSortingRepository<Poll, Long> {
   
