893cf635f5ac6546db46dd7c472cd55383db204a
Paola <camachopaola311@gmail.com>
2020-08-07 16:56:16 -0400

    Fixed more errors

---
+++ src/main/java/com/stayready/poll_application/controller/PollController.java
@@ -0,0 +1,26 @@
+package com.stayready.poll_application.controller;
+
+import com.stayready.poll_application.domain.Poll;
+import com.stayready.poll_application.repositories.PollRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PollController {
+    private PollRepository pollRepository;
+    @Autowired
+    public PollController(PollRepository pollRepository){
+        this.pollRepository = pollRepository;
+    }
+    @RequestMapping(value="/polls", method= RequestMethod.GET)
+    public ResponseEntity<Iterable<Poll>> getAllPolls() {
+        Iterable<Poll> allPolls = pollRepository.findAll();
+        return new ResponseEntity<>(allPolls, HttpStatus.OK);
+    }
+
+
+}
