9ce1cabf0c3a4152a42e6eab0d3ed37ba833e2d7
Kaveesha <weerasirikaveesha@gmail.com>
2020-08-06 20:17:04 -0500

    finished base part 5

--- src/main/java/com/stayready/poll_application/handler/RestExceptionHandler.java
+++ src/main/java/com/stayready/poll_application/handler/RestExceptionHandler.java
@@ -3,12 +3,17 @@ package com.stayready.poll_application.handler;
 import com.stayready.poll_application.dtos.error.ValidationError;
 import com.stayready.poll_application.error.ErrorDetail;
 import com.stayready.poll_application.exception.ResourceNotFoundException;
+import com.stayready.poll_application.repositories.VoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -17,13 +22,22 @@ import java.util.List;
 
 @ControllerAdvice
 public class RestExceptionHandler {
+    private MessageSource messageSource;
+
+    @Autowired
+    public RestExceptionHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleValidationError(MethodArgumentNotValidException manve, HttpServletRequest request) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public @ResponseBody ErrorDetail handleValidationError(MethodArgumentNotValidException manve, HttpServletRequest request) {
         ErrorDetail errorDetail = new ErrorDetail();
+        // Populate errorDetail instance
         errorDetail.setTimeStamp(new Date().getTime());
         errorDetail.setStatus(HttpStatus.BAD_REQUEST.value());
-        String requestPath = (String) request.getAttribute("javax.servlet.error,request_uri");
+        String requestPath = (String) request.getAttribute("javax.servlet.error.request_uri");
         if(requestPath == null) {
             requestPath = request.getRequestURI();
         }
@@ -41,9 +55,9 @@ public class RestExceptionHandler {
             }
             ValidationError validationError = new ValidationError();
             validationError.setCode(fe.getCode());
-            validationError.setMessage(fe.getDefaultMessage());
+            validationError.setMessage(messageSource.getMessage(fe, null));
             validationErrorList.add(validationError);
         }
-        return new ResponseEntity<>(errorDetail, null, HttpStatus.BAD_REQUEST);
+        return errorDetail;
     }
 }
