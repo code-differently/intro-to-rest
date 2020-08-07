63321a5bc48784a707bfb1f6c9b58768d52995cb
Solomon Jackson <solomonjackson@solomons-air.myfiosgateway.com>
2020-08-07 13:11:24 -0400

    Completed lab.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,24 @@
 insert into poll (poll_id, question) values (1, 'What is your favorite color?');
 insert into option (option_id, option_value, poll_id) values (1, 'Red', 1);
+insert into option (option_id, option_value, poll_id) values (2, 'Blue', 1);
+insert into option (option_id, option_value, poll_id) values (3, 'Green', 1);
+
+insert into poll (poll_id, question) values (2, 'Favorite Upside Down Creature?');
+insert into option (option_id, option_value, poll_id) values (4, 'Demogorgon', 2);
+insert into option (option_id, option_value, poll_id) values (5, 'DemoDog', 2);
+insert into option (option_id, option_value, poll_id) values (6, 'MindFlayer', 2);
+
+insert into poll (poll_id, question) values (3, 'Pick a Starter');
+insert into option (option_id, option_value, poll_id) values (7, 'Fire', 3);
+insert into option (option_id, option_value, poll_id) values (8, 'Water', 3);
+insert into option (option_id, option_value, poll_id) values (9, 'Grass', 3);
+
+insert into poll (poll_id, question) values (4, 'Pick a Band');
+insert into option (option_id, option_value, poll_id) values (10, 'Marching Band', 4);
+insert into option (option_id, option_value, poll_id) values (11, 'Concert Band', 4);
+insert into option (option_id, option_value, poll_id) values (12, 'Jazz Band', 4);
+
+insert into poll (poll_id, question) values (5, 'Favorite Meal');
+insert into option (option_id, option_value, poll_id) values (13, 'Breakfast', 5);
+insert into option (option_id, option_value, poll_id) values (14, 'Lunch', 5);
+insert into option (option_id, option_value, poll_id) values (15, 'Dinner', 5);
